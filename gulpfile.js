58a344761c81d6cc03d35c92b6f787c8cb9e6e69
Sergey Zhuravlev <Zhuravlev@rutoken.ru>
2020-07-13 16:46:57 +0300

    Replace tabs with spaces in gulp files

--- gulpfile.js
+++ gulpfile.js
@@ -6,42 +6,42 @@ var browserify = require('browserify'),
     uglify = require('gulp-uglify-es').default;
 
 gulp.task('clean', function () {
-	return del(['build']);
+    return del(['build']);
 });
 
 gulp.task('pages', function () {
-	return gulp.src('src/*.html')
-		.pipe(gulp.dest('build'));
+    return gulp.src('src/*.html')
+        .pipe(gulp.dest('build'));
 });
 
 gulp.task('libs', function () {
-	return gulp.src('src/libs/**')
-		.pipe(gulp.dest('build/libs'));
+    return gulp.src('src/libs/**')
+        .pipe(gulp.dest('build/libs'));
 });
 
 gulp.task('deps', gulp.series('libs', function () {
-	return browserify('src/dependencies.js')
-		.bundle()
-		.pipe(source('dependencies.js'))
-		.pipe(buffer())
-		.pipe(uglify())
+    return browserify('src/dependencies.js')
+        .bundle()
+        .pipe(source('dependencies.js'))
+        .pipe(buffer())
+        .pipe(uglify())
         .on('error', console.error)
-		.pipe(gulp.dest('build/'));
+        .pipe(gulp.dest('build/'));
 }));
 
 gulp.task('scripts', gulp.series('deps', function () {
-	return gulp.src(['src/present.js', 'src/cmc.js'])
-		.pipe(gulp.dest('build/'));
+    return gulp.src(['src/present.js', 'src/cmc.js'])
+        .pipe(gulp.dest('build/'));
 }));
 
 gulp.task('styles', function () {
-	return gulp.src('src/*.css')
-		.pipe(gulp.dest('build/'));
+    return gulp.src('src/*.css')
+        .pipe(gulp.dest('build/'));
 });
 
 gulp.task('images', function () {
-	return gulp.src('src/images/*.png')
-		.pipe(gulp.dest('build/images'));
+    return gulp.src('src/images/*.png')
+        .pipe(gulp.dest('build/images'));
 });
 
 gulp.task('default', gulp.series('clean', 'pages', 'scripts', 'styles', 'images', function (done) {

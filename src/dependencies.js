c0c1e79db4da07e78533163316b2d826000f5f3d
Dmitry Meshkov <meshkov@rutoken.ru>
2021-11-22 19:04:34 +0300

    Use new version of rutoken-js in demopage
    Refer: PLUGIN-643

--- src/dependencies.js
+++ src/dependencies.js
@@ -1,5 +1,5 @@
 require('es6-promise').polyfill();
 require('console-polyfill');
-global.rutoken = require('rutoken');
+global.rutoken = require('@aktivco/rutoken-plugin');
 global.asn1js = require('asn1js');
 global.pkijs = require('pkijs');

741040b71ef52accc83f16479318d2c766f4ce41
Kirill Ermoshin <ermoshin@rutoken.ru>
2025-05-27 16:15:55 +0300

    Change signature size for secp521r1 to 1056 for gen key pair
    signatureSize option was introduced in 58309e1c and now it looks
    like as obsolete parameter. Signature is r (66 bytes) and
    s (66 bytes) concatenation so result size is 132 bytes (1056 bits)

--- src/present.js
+++ src/present.js
@@ -1648,7 +1648,7 @@ var TestSuite = new(function () {
                 if (options.paramset === "secp384r1") {
                     options.signatureSize = 768;
                 } else if (options.paramset === "secp521r1") {
-                    options.signatureSize = 1042;
+                    options.signatureSize = 1056;
                 } else {
                     options.signatureSize = 512;
                 }
